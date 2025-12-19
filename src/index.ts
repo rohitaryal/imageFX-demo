3ad1aa83261ee22eb42ecad406274c9841e15965
rohitaryal <erucix@gmail.com>
2025-12-20 05:18:41 +0530

    fix: class instantiation inside req

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,18 @@ import { Hono } from 'hono'
 import { cors } from 'hono/cors';
 import { logger } from 'hono/logger';
 
-const app = new Hono()
-const fx = new ImageFX(process.env.COOKIE || "")
+interface Binding {
+    COOKIE: string;
+}
+
+const app = new Hono<{ Bindings: Binding }>();
 
 app.use(cors())
 app.use(logger());
 
 app.get("/generate", async (c) => {
+    const fx = new ImageFX(c.env.COOKIE);
+
     let prompt = c.req.query("prompt");
     if (!(prompt?.trim())) {
         return c.text("MISSING PROMPT", 400);
