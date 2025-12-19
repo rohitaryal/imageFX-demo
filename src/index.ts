dde4e02ab8764165d033917c038ad1f1c82bece6
rohitaryal <erucix@gmail.com>
2025-12-20 05:08:35 +0530

    add generate route

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,24 @@
+import { ImageFX } from '@rohitaryal/imagefx-api';
 import { Hono } from 'hono'
+import { cors } from 'hono/cors';
+import { logger } from 'hono/logger';
 
 const app = new Hono()
+const fx = new ImageFX(process.env.COOKIE || "")
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.use(cors())
+app.use(logger());
+
+app.get("/generate", async (c) => {
+    let prompt = c.req.query("prompt");
+    if (!(prompt?.trim())) {
+        return c.text("MISSING PROMPT", 400);
+    }
+
+    prompt = decodeURI(prompt);
+
+    const generatedImage = await fx.generateImage(prompt);
+    return c.text(generatedImage[0].encodedImage);
+});
 
 export default app
